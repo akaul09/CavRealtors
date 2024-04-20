1a84069b24f2e3549175c2fca6049c4dc5ca3b21
rachitgoli <zex8py@virginia.edu>
2024-04-20 19:15:54 -0400

    fixed addProperty ui

--- addProperty.php
+++ addProperty.php
@@ -17,137 +17,81 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/[email protected]/dist/css/bootstrap.min.css" integrity="sha256-" crossorigin="anonymous">
-
-    <!-- Bootstrap JS, Popper.js, and jQuery -->
-    <script src="https://cdn.jsdelivr.net/npm/[email protected]/dist/js/bootstrap.bundle.min.js" integrity="sha256-gOQJIa9+K/" crossorigin="anonymous"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Add Property Form</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-
 <body>
 
-    <div class="container">
-        <div class="row g-3 mt-2">
-            <div class="col">
-                <h2>Add Property</h2>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>CavRealtors</span>
+                    <button class="btn btn-primary">Back</button>
+                </div>
+                <div class="col-md-4">
+                    <img src="/Users/rachitgoli/databases/assets/cavrealtors-high-resolution-logo-transparent 1.png"  class="img-fluid">
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="houseStyle">HOUSESTYLE</label>
+                            <input type="text" class="form-control" id="housestyle", name = "housestyle" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="price">PRICE</label>
+                            <input type="text" class="form-control" id="price", name = "price" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="address">ADDRESS</label>
+                            <input type="text" class="form-control" id="address", name = "address" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="state">STATE</label>
+                            <input type="text" class="form-control" id="state", name = "state" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="city">CITY</label>
+                            <input type="text" class="form-control" id="city", name = "city" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="country">COUNTRY</label>
+                            <select class="form-control" id="country">
+                                <option>United States</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="status">STATUS OF PROPERTY</label>
+                            <input type="text" class="form-control" id="status", name = "status" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="bed">BED</label>
+                            <input type="text" class="form-control" id="bed", name = "beds", required>
+                        </div>
+                        <div class="form-group">
+                            <label for="baths">BATHS</label>
+                            <input type="text" class="form-control" id="baths", name = "baths" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="sqft">SQUAREFEET</label>
+                            <input type="text" class="form-control" id="sqft", name = "sqft" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
+    </div>
+</div>
 
-
-        <hr />
-        <div class="container">
-
-            <form class="addProp" method="post" action="addProperty.php">
-                
-            <div class="mb-3">
-                    <label for="houseStyle" class="form-label">House Style</label>
-                    <input type="text" class="form-control" id="housestyle" name="housestyle" required>
-                    <div class="invalid-feedback">
-                        Please provide the style of the house.
-                    </div>
-                </div>
-                <br><br>
-                <div class="mb-3">
-                    <label for="price" class="form-label">Price</label>
-                    <input type="number" class="form-control" id="price" name="price" required>
-                    <div class="invalid-feedback">
-                        Please provide the Price of the house.
-                    </div>
-                </div>
-                <br><br>
-                <!-- <div class="mb-3">
-                    <label for="address" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="address" name="address" required>
-                    <div class="invalid-feedback">
-                        Please provide the address.
-                    </div>
-                </div>
-                <br><br>
-
-                <div class="mb-3">
-                    <label for="brokerName" class="form-label">Broker Name</label>
-                    <input type="text" class="form-control" id="brokerName" name="brokerName" required>
-                    <div class="invalid-feedback">
-                        Please provide the name of the broker.
-                    </div>
-                </div>
-                <br><br>
-
-                <div class="mb-3">
-                    <label for="bathrooms" class="form-label">No. of Bathrooms</label>
-                    <input type="number" class="form-control" id="bathrooms" name="bathrooms" required>
-                    <div class="invalid-feedback">
-                        Please provide the number of bathrooms.
-                    </div>
-                </div>
-                <br><br>
-
-                <div class="mb-3">
-                    <label for="bedrooms" class="form-label">No. of Bedrooms</label>
-                    <input type="number" class="form-control" id="bedrooms" name="bedrooms" required>
-                    <div class="invalid-feedback">
-                        Please provide the number of bedrooms.
-                    </div>
-                </div>
-                <br><br>
-
-                <div class="mb-3">
-                    <label for="squareFeet" class="form-label">Sq ft.</label>
-                    <input type="number" class="form-control" id="squareFeet" name="squareFeet" required>
-                    <div class="invalid-feedback">
-                        Please provide the square footage.
-                    </div>
-                </div>
-                <br><br>
-
-
-                <div class="mb-3">
-                    <label for="state" class="form-label">State</label>
-                    <input type="text" class="form-control" id="state" name="state" required>
-                    <div class="invalid-feedback">
-                        Location Information (State)
-                    </div>
-                </div>
-                <br><br>
-
-                <div class="mb-3">
-                    <label for="county" class="form-label">County</label>
-                    <input type="text" class="form-control" id="county" id="county" required>
-                    <div class="invalid-feedback">
-                        Please provide the county.
-                    </div>
-                </div>
-                <br><br>
-
-                <div class="mb-3">
-                    <label for="status" class="form-label">Status</label>
-                    <select class="form-select" id="status" name="status" required>
-                        <option selected disabled value="">Choose...</option>
-                        <option value="for_sale">For Sale</option>
-                        <option value="sold">Sold</option>
-                        <option value="pending">Pending</option>
-                        <option value="not_available">Not Available</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please select the status of the house.
-                    </div>
-                </div>
-                <br><br>
-                <br><br>-->
-
-                <button class="btn btn-primary" type="submit">Submit form</button> 
-            </form>
-        </div>
-
-
-        <br /><br /> 
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
-
 </html>
